f10dffb8e28dd61a9741a9311c2fa46cba82dfe7
YOUR_NAME <YOUR_EMAIL>
2022-03-16 23:51:59 +0300

    second_commit(everything works)

---
+++ telebot/sendmessage.py
@@ -0,0 +1,16 @@
+import requests
+from .models import TeleSetiings
+
+
+def sendTelegram():
+	settings = TeleSetiings.objects.get(pk=1)
+	token = str(settings.tg_token)
+	chat_id = str(settings.tg_chat)
+	text = str(settings.tg_message)
+	api = 'https://api.telegram.org/bot'
+	method = api + token + '/sendMessage'
+
+	req = requests.post(method, data={
+		'chat_id': chat_id,
+		'text': text
+		})
